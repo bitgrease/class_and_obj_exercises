04b3f26ae9686e7f168bde523d3b2801fb98c9a1
Kelly Netterville <bitgrease@gmail.com>
2018-01-05 15:48:08 -0500

    Initial commit with comments and instructions.

---
+++ ex1.rb
@@ -0,0 +1,5 @@
+=begin
+  Create a class MyCar
+  Instance variables: year, color, model, speed (set to 0 on init)
+  create instance methods to accelerate, brake, and shut off (and start?) the car.
+=end
