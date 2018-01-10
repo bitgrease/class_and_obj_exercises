a9c8e22a55e691d8227fd0eab2ab2db3b9e4cee0
Kelly Netterville <bitgrease@gmail.com>
2018-01-10 10:07:42 -0500

    Finish of Classes and Objects lecture exercises.

--- lecture_exes.rb
+++ lecture_exes.rb
@@ -1,57 +1,53 @@
 =begin
-  Given the code below (testcases), code the class definition
-=end
+  
+  input a name of one or two words.
+  Output = if one word, assign to first_name and '' to second, if two words, assign first to firstname
+    and second word to last_name
 
-# class Person
+=end 
 
-#   attr_accessor :name
-#   def initialize(name)
-#     @name = name
-#   end
-# end
-
-# bob = Person.new('bob')
-# p bob.name  == 'bob'                # => 'bob'
-# bob.name = 'Robert'
-# p bob.name == 'Robert'              # => 'Robert'
 
 class Person
-
   attr_accessor :first_name, :last_name
-  def initialize(first_name='', last_name='')
-    @first_name = first_name
-    @last_name = last_name
+
+  def initialize(full_name)
+    parse_full_name(full_name)
   end
 
   def name
-    if last_name.size > 0
-      "#{first_name} #{last_name}"
+    "#{first_name} #{last_name}".strip
+  end
+
+  def name=(full_name) # <== Problem here. Approach like exercise for interview
+    parse_full_name(full_name)
+  end
+
+  def parse_full_name(name)
+    parts = name.split
+    self.first_name = parts.first
+    case parts.size
+    when 1 
+      self.last_name = ''
     else
-      "#{first_name}"
+      self.last_name = parts.last
     end
   end
+
+  def to_s
+    first_name
+  end
+
+  private :parse_full_name
 end
 
-
 bob = Person.new('Robert')
-p bob.name == 'Robert'                  # => 'Robert'
-p bob.first_name  == 'Robert'          # => 'Robert'
-p bob.last_name   == ''          # => ''
+p bob.name                 == 'Robert' # => 'Robert'
+p bob.first_name           == 'Robert' # => 'Robert'
+p bob.last_name             == ''# => ''
 bob.last_name = 'Smith'
-p bob.name     == 'Robert Smith'
-            
+p bob.name                 == 'Robert Smith' # => 'Robert Smith'
 
-# LS Solution
-# class Person
-#   attr_accessor :first_name, :last_name
-
-#   def initialize(full_name)
-#     parts = full_name.split
-#     @first_name = parts.first
-#     @last_name = parts.size > 1 ? parts.last : ''
-#   end
-
-#   def name
-#     "#{first_name} #{last_name}".strip
-#   end
-# end
+bob.name = "John Adams"
+p bob.first_name          #== 'John'  # => 'John'
+p bob.last_name            #== 'Adams' # => 'Adams'
+puts "This person's name is #{bob}." 
